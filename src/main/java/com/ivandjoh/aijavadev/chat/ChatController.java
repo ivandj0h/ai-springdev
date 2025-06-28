801297fcdf4f53181d51b502fff20e2b4df19407
ivandjoh <ivandj0h@users.noreply.github.com>
2025-06-28 17:41:21 +0700

    add chat functionality

--- src/main/java/com/ivandjoh/aijavadev/chat/ChatController.java
+++ src/main/java/com/ivandjoh/aijavadev/chat/ChatController.java
@@ -1,7 +1,9 @@
 package com.ivandjoh.aijavadev.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -12,5 +14,20 @@ public class ChatController {
         this.chatClient = builder.build();
     }
 
+    @GetMapping("/chat")
+    public String chat() {
+        return chatClient.prompt()
+                .user("Tell me an interesting fact about spring boot")
+                .call()
+                .content();
+    }
 
+    @GetMapping("/stream")
+    public Flux<String> stream() {
+        return chatClient.prompt()
+                .user("i am visiting jakarta, " +
+                        "can you tell me 10 interesting eating places that i have to visit?")
+                .stream()
+                .content();
+    }
 }
